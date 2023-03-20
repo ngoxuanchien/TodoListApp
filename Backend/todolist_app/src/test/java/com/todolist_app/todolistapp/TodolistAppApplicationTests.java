58f69fe0391e5b9eec41c6a6f711bce28f4fdb8d
ngoxuanchien <ngoxuanchien9a@gmail.com>
2023-03-20 23:40:35 +0700

    first commit

---
+++ Backend/todolist_app/src/test/java/com/todolist_app/todolistapp/TodolistAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.todolist_app.todolistapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
