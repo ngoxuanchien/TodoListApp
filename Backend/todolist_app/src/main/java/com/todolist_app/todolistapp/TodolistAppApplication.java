1721d5bcab81b5f8d3bd490c7be379771b32d82c
ngoxuanchien <ngoxuanchien9a@gmail.com>
2023-04-01 20:54:09 +0700

    test mobile app

--- Backend/todolist_app/src/main/java/com/todolist_app/todolistapp/TodolistAppApplication.java
+++ Backend/todolist_app/src/main/java/com/todolist_app/todolistapp/TodolistAppApplication.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TodolistAppApplication {
 
+	
 	public static void main(String[] args) {
 		SpringApplication.run(TodolistAppApplication.class, args);
 	}
