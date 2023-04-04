710dbc0eed6487a00ed8b54492fa501a1d4c4e10
ngoxuanchien <ngoxuanchien9a@gmail.com>
2023-04-05 00:45:06 +0700

    sign up activity

--- MobileApp/ToDo/app/src/main/java/com/example/todo/SignInActivity.java
+++ MobileApp/ToDo/app/src/main/java/com/example/todo/SignInActivity.java
@@ -1,7 +1,7 @@
 package com.example.todo;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.view.View;
 import android.widget.Button;
@@ -11,8 +11,6 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
 import com.example.todo.Helpers.StringHelper;
@@ -23,7 +21,8 @@ public class SignInActivity extends AppCompatActivity {
 
     Button _signInBtn;
     EditText _email, _password;
-    @SuppressLint("MissingInflatedId")
+    final String url = StringHelper.url + "/api/v1/auth/authenticate";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,21 +33,18 @@ public class SignInActivity extends AppCompatActivity {
 
         _signInBtn = findViewById(R.id.sign_in_btn);
 
-        _signInBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                try {
-                    authenticatedUser();
-                } catch (JSONException e) {
-                    throw new RuntimeException(e);
-                }
+        _signInBtn.setOnClickListener(view -> {
+            try {
+                authenticatedUser();
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
             }
         });
     }
 
     private boolean validateEmail() {
         String email = _email.getText().toString();
-        boolean result = true;
+        boolean result;
 
         if (email.isEmpty()) {
             _email.setError("Email cannot empty!");
@@ -66,7 +62,7 @@ public class SignInActivity extends AppCompatActivity {
 
     private boolean validatePassword() {
         String password = _password.getText().toString();
-        boolean result = true;
+        boolean result;
 
         if (password.isEmpty()) {
             _password.setError("Password cannot be empty!");
@@ -80,11 +76,7 @@ public class SignInActivity extends AppCompatActivity {
     }
 
     private void authenticatedUser() throws JSONException {
-        if (validateEmail() || validatePassword()) {
-            String url = StringHelper.url + "/api/v1/auth/authenticate";
-
-            RequestQueue queue = Volley.newRequestQueue(SignInActivity.this);
-
+        if (validateEmail() && validatePassword()) {
             JSONObject jsonObject = new JSONObject();
             jsonObject.put("email", _email.getText());
             jsonObject.put("password", _password.getText());
@@ -93,33 +85,35 @@ public class SignInActivity extends AppCompatActivity {
                     Request.Method.POST,
                     url,
                     jsonObject,
-                    new Response.Listener<JSONObject>() {
-                        @Override
-                        public void onResponse(JSONObject response) {
-                            SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
-                            SharedPreferences.Editor editor = sharedPref.edit();
-                            try {
-                                editor.putString("jwtToken", response.getString("token"));
-                            } catch (JSONException e) {
-                                throw new RuntimeException(e);
-                            }
-                            editor.apply();
+                    response -> {
+                        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPref.edit();
+                        try {
+                            editor.putString("jwtToken", response.getString("token"));
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
                         }
-                    }, new Response.ErrorListener() {
-                @Override
-                public void onErrorResponse(VolleyError error) {
-                    System.out.println(error.getMessage());
-                    Toast.makeText(SignInActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
-                }
-            });
+                        editor.apply();
+                    }, error -> {
+                        System.out.println(error.getMessage());
+                        Toast.makeText(SignInActivity.this, "Login Failed", Toast.LENGTH_SHORT).show();
+                    });
 
             RequestQueue requestQueue = Volley.newRequestQueue(SignInActivity.this);
             requestQueue.add(jsonObjectRequest);
-        } else {
-            // do nothing
-        }
+        }  // do nothing
+
     }
 
     public void gotoSignUpAct(View view) {
+        Intent intent = new Intent(SignInActivity.this, SignUpActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void goToHome(View view) {
+        Intent intent = new Intent(SignInActivity.this, MainActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
--- MobileApp/ToDo/app/src/main/java/com/example/todo/SignUpActivity.java
+++ MobileApp/ToDo/app/src/main/java/com/example/todo/SignUpActivity.java
@@ -1,15 +1,28 @@
 package com.example.todo;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.example.todo.Helpers.StringHelper;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class SignUpActivity extends AppCompatActivity {
 
     EditText _firstName, _lastName, _email, _password, _confirm;
     Button _signUpBtn;
+    final String url = StringHelper.url + "/api/v1/auth/register";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,19 +37,125 @@ public class SignUpActivity extends AppCompatActivity {
 
         _signUpBtn = findViewById(R.id.sign_up_btn);
 
-        _signUpBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
+        _signUpBtn.setOnClickListener(view -> {
+            try {
                 processFormFields();
+            } catch (JSONException e) {
+                throw new RuntimeException(e);
             }
         });
     }
 
-    private boolean validatedEmail() {
-        return false;
+    private boolean validateEmail() {
+        String email = _email.getText().toString();
+        boolean result;
+
+        if (email.isEmpty()) {
+            _email.setError("Email cannot empty!");
+            result = false;
+        } else if (!StringHelper.regexEmailValidationPattern(email)) {
+            _email.setError("Please enter a valid email!");
+            result = false;
+        } else {
+            _email.setError(null);
+            result = true;
+        }
+
+        return result;
     }
 
-    private void processFormFields() {
+    private boolean validateFirstName() {
+        boolean result;
+        String firstName = _firstName.getText().toString();
 
+        if (firstName.isEmpty()) {
+            _firstName.setError("First name cannot empty");
+            result = false;
+        } else {
+            _firstName.setError(null);
+            result = true;
+        }
+
+        return result;
+    }
+
+    public boolean validateLastName() {
+        boolean result;
+        String lastName = _lastName.getText().toString();
+
+        if (lastName.isEmpty()) {
+            _lastName.setError("Last name cannot empty");
+            result = false;
+        } else {
+            _lastName.setError(null);
+            result = true;
+        }
+
+        return result;
+    }
+
+    public boolean validatePasswordAndConfirm() {
+        boolean result;
+        String password = _password.getText().toString();
+        String confirm = _confirm.getText().toString();
+
+        if (password.isEmpty()) {
+            _password.setError("Password cannot be empty");
+            result = false;
+        } else if (!password.equals(confirm)) {
+            _password.setError("Password do not match!");
+            result = false;
+        } else {
+            _password.setError(null);
+            _confirm.setError(null);
+            result = true;
+        }
+
+        return result;
+    }
+
+    private void processFormFields() throws JSONException {
+
+        if (validateFirstName() && validateLastName() && validateEmail() && validatePasswordAndConfirm()) {
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("firstname", _firstName.getText());
+            jsonObject.put("lastname", _lastName.getText());
+            jsonObject.put("email", _email.getText());
+            jsonObject.put("password", _password.getText());
+
+            JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(
+                    Request.Method.POST,
+                    url,
+                    jsonObject,
+                    response -> {
+                        SharedPreferences sharedPref = getPreferences(Context.MODE_PRIVATE);
+                        SharedPreferences.Editor editor = sharedPref.edit();
+                        try {
+                            editor.putString("jwtToken", response.getString("token"));
+                        } catch (JSONException e) {
+                            throw new RuntimeException(e);
+                        }
+                        editor.apply();
+                    }, error -> {
+                        System.out.println(error.getMessage());
+                        Toast.makeText(SignUpActivity.this, "Register error", Toast.LENGTH_SHORT).show();
+                    });
+
+            RequestQueue requestQueue = Volley.newRequestQueue(SignUpActivity.this);
+            requestQueue.add(jsonObjectRequest);
+        }  // do nothing;
+
+    }
+
+    public void goToSignInAct(View view) {
+        Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void goToHome(View view) {
+        Intent intent = new Intent(SignUpActivity.this, SignInActivity.class);
+        startActivity(intent);
+        finish();
     }
 }
